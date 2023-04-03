ac2d14e3f6e963908b707aca7e6ea70f5cff8ba6
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-04-03 22:04:59 +0200

    javascript functions added

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const displayCalc = document.getElementById("displayCalc");
+
+let accumulator=0;
+let currentValue = 0;
+let operand = '';
+let convertedNumber=0;
+let count =0;
 function updateDisplay(number){
     
     displayCalc.value+=number;
@@ -6,7 +12,58 @@ function updateDisplay(number){
 function addDot(){
     displayCalc.value+='.';
 }
+function resetDisplay(){
+    displayCalc.value='';
+    accumulator = 0;
+    count =0;
+}
 
 function clearDisplay(){
     displayCalc.value='';
+    
+}
+
+function operation(symbol){
+    operand = symbol;
+    if(count === 0){
+        accumulator = parseFloat(displayCalc.value);
+    }
+    else{
+        switch(symbol){
+            case "+":
+            accumulator += parseFloat(displayCalc.value);
+                break;
+            case "-":
+                accumulator = accumulator - parseFloat(displayCalc.value);
+                break;
+            case "*":
+                accumulator = accumulator * parseFloat(displayCalc.value);
+                break;
+            case "/":
+                
+                accumulator = accumulator / parseFloat(displayCalc.value);
+                break;
+            default:
+                console.log("nessuna operazione");
+        }
+    }    
+    console.log(accumulator)
+    displayCalc.value='';
+    count +=1;
+}
+
+
+function equal(){
+    if(displayCalc.value ==''){
+        displayCalc.value = accumulator.toLocaleString('en-US');
+    }
+    else{
+       
+        operation(operand);
+        displayCalc.value= accumulator.toLocaleString('en-US');
+        console.log(accumulator);
+    }
+    count +=1;
+    
+    
 }
