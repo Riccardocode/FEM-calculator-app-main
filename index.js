d21434daf7751c86b9b105ea451de4f20009660f
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-04-04 15:45:24 +0200

    colors converted to variables for theme 1 and 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const displayCalc = document.getElementById("displayCalc");
 const selectedTheme = document.getElementById("selection");
+const variables = document.querySelector(':root');
 const choise1 = document.getElementById("choise1");
 const choise2 = document.getElementById("choise2");
 const choise3 = document.getElementById("choise3");
@@ -49,7 +50,6 @@ function operation(symbol){
                 accumulator = accumulator * parseFloat(displayCalc.value);
                 break;
             case "/":
-                
                 accumulator = accumulator / parseFloat(displayCalc.value);
                 break;
             default:
@@ -75,13 +75,68 @@ function equal(){
     count +=1;    
 }
 
+function changeColors(theme){
+    let changeColor;
+    let support;
+
+    support = getComputedStyle(variables);
+    
+    changeColor = support.getPropertyValue(`--buttonsT${theme}`);
+    variables.style.setProperty("--buttons", changeColor );
+    
+    changeColor = support.getPropertyValue(`--text-buttons${theme}`);
+    variables.style.setProperty("--text-buttons", changeColor);
+
+    changeColor = support.getPropertyValue(`--buttons-hover${theme}`);
+    variables.style.setProperty("--buttons-hover", changeColor);
+    
+    changeColor = support.getPropertyValue(`--pressDelT${theme}`);
+    variables.style.setProperty("--buttonDel", changeColor);
+    
+    changeColor = support.getPropertyValue(`--backgroundColorT${theme}`);
+    variables.style.setProperty("--backgroundColor", changeColor);
+
+    changeColor = support.getPropertyValue(`--topNumbers${theme}`);
+    variables.style.setProperty("--topNumbers", changeColor);
+
+    changeColor = support.getPropertyValue(`--selectorBar-background${theme}`);
+    variables.style.setProperty("--selectorBar-background", changeColor);
+
+    changeColor = support.getPropertyValue(`--selectorCircle${theme}`);
+    variables.style.setProperty("--selectorCircle", changeColor);
+    
+    changeColor = support.getPropertyValue(`--shadowButton${theme}`);
+    variables.style.setProperty("--shadowButton", changeColor);
+
+    changeColor = support.getPropertyValue(`--displayCalc-background${theme}`);
+    variables.style.setProperty("--displayCalc-background", changeColor);
+
+    changeColor = support.getPropertyValue(`--button-background${theme}`);
+    variables.style.setProperty("--button-background", changeColor);
+
+    changeColor = support.getPropertyValue(`--button-DR-background-hover${theme}`);
+    variables.style.setProperty("--button-DR-background-hover", changeColor);
+   
+    changeColor = support.getPropertyValue(`--button-equal-shadow-background-hover${theme}`);
+    variables.style.setProperty("--button-equal-shadow-background-hover", changeColor);
+    
+    changeColor = support.getPropertyValue(`--button-equal-background-hover${theme}`);
+    variables.style.setProperty("--button-equal-background-hover", changeColor);
+    
+    changeColor = support.getPropertyValue(`--text-hover${theme}`);
+    variables.style.setProperty("--text-hover", changeColor);
+
+}
+
 function changeTheme1(){
     selectedTheme.style.left="0px";
+    changeColors(1);
     console.log("changed to 1");
 }
 
 function changeTheme2(){
     selectedTheme.style.left="22px";
+    changeColors(2);   
     console.log("changed to 2");
 }
 
@@ -89,3 +144,4 @@ function changeTheme3(){
     selectedTheme.style.left="46px";
     console.log("changed to 3");
 }
+
